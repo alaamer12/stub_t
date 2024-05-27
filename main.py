eca0c040b49a5206c5dc06095fca9a11504875b6
alaamer12 <ahmedmuhmmed239@gmail.com>
2024-05-27 23:01:38 +0300

    update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import utils.math_functions as mf
 
-mf.add(1, 2)
-mf.subtract(1, 2)
-
-import getpass
-
-pwd = getpass.getpass(prompt='Enter the password')
-if pwd == 'Admin':
-    print('Unlock!')
-else:
-    print('You entered wrong password')
+print(mf.add(1, 2))
+print(mf.subtract(1, 2))
+#
+# import getpass
+#
+# pwd = getpass.getpass(prompt='Enter the password')
+# if pwd == 'Admin':
+#     print('Unlock!')
+# else:
+#     print('You entered wrong password')
